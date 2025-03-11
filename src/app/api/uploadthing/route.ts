17071831ad45a534f09ca2b1286b183c650d1df3
Natoons <nadirtounsifr@gmail.com>
2025-03-11 18:18:50 +0100

    feat: add debug

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,11 +2,18 @@ import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
-// Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
     router: ourFileRouter,
     config: {
+        // Passez explicitement votre token (si nécessaire) :
+        token: process.env.UPLOADTHING_TOKEN,
+        // Indique si on est en dev ou pas :
         isDev: process.env.NODE_ENV === "development",
-        callbackUrl: process.env.UPLOADTHING_CALLBACK_URL,
+        // URL complète si la détection automatique échoue :
+        callbackUrl: "https://www.workyt.fr/api/uploadthing",
+        // Niveau de logs (Error, Warning, Info, Debug, Trace) :
+        logLevel: "Debug",
+        // Format de logs (json, logFmt, structured, pretty)
+        logFormat: "json",
     },
 });
