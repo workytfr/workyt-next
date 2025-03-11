a5be6d2338189512e018528b8b668152f40a0dfd
Natoons <nadirtounsifr@gmail.com>
2025-03-11 09:35:35 +0100

    feat: add callbackUrl

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -7,5 +7,6 @@ export const { GET, POST } = createRouteHandler({
     router: ourFileRouter,
     config: {
         isDev: process.env.NODE_ENV === "development",
+        callbackUrl: process.env.UPLOADTHING_CALLBACK_URL,
     },
 });
